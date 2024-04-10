850c98098d398ba4525a5f045af4607e088e1b76
MarvsConcept <marvade8@gmail.com>
2024-04-10 20:33:23 +0100

    Pushing to Render

--- tododrf/settings.py
+++ tododrf/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR/'assets'
 
-STATICFILES_STORAGE = "whitenoise.storge.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
